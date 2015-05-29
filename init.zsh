aeafac8e1cdba3d6d6b40cfc437c6e5860b03718
Morgan Williams <wowzapollo@gmail.com>
2015-05-29 02:15:15 -0600

    quick commit - minor changes

--- init.zsh
+++ init.zsh
@@ -1,41 +1,55 @@
-# Git: Make rsa key & copy to clipboard
-make_rsa(){
-  GIT_EMAIL=$(git config --global user.email)
-  echo "\nYou can just push enter on these\n"
-  # todo: Config email if doesn't exist
-  # todo: cd to ~/.ssh/
-  ssh-keygen -t rsa -b 4096 -C "$GIT_EMAIL"
+# download & run this file to install everything!
 
-  # copy clipboard
-  pbcopy < ~/.ssh/id_rsa.pub
-  echo "Copied key to clipboard."
-  echo "Opening browser"
-  open https://github.com/settings/ssh
-
-}
 
 # OSX customizations
 # --------------------------------------
 if [[ $(uname) == "Darwin" ]]; then
+
+  newMac() {
+    # Set up screenshots
+    mkdir ~/Pictures/Screenshots
+    screendir ~/Pictures/Screenshots
+    screenname 
+
+    # enable select in QuickLook
+    defaults write com.apple.finder QLEnableTextSelection TRUE
+
+    # Hide apps that I rarely use.
+    # Automator, Chess, Dashboard, DVD Player, Font Book, Game Center,
+    # Image Capture, Launchpad, Mail, Mission Control, Photo Booth, Stickies, TextEdit
+    buryApps
+  }
+
+
+  # change screenshot directory
+  screendir () {
+    # todo: see if directory exists & create if it doesn't
+    defaults write com.apple.screencapture location $@; killall SystemUIServer
+  }
+
+
+  screenname () {
+    defaults write com.apple.screencapture name $1; killall SystemUIServer
+  }
+#
+
   # Move the apps you never use to /Applications/Utilities
   # ex: TextEdit || TextEdit.app || /Applications/TextEdit.app
   buryApp() {
     for x in $@; do
       app=$(basename "$x")
-      # extension="${app##*.}"
       app="${app%.*}"
-      # echo " Utilities/$app"
-
       sudo mv /Applications/$app.app /Applications/Utilities/$app.app
     done
-
   }
+
 
   buryApps() {
     $(buryApp Automator Chess \ Dashboard DVD\ Player Font\ Book Game\ Center \
     Image\ Capture Launchpad Mail Mission\ Control Photo\ Booth Stickies TextEdit \
      Time\ Machine)
   }
+
 
   # turn on tab completion for brew
   brewTab() {
@@ -44,3 +58,21 @@ if [[ $(uname) == "Darwin" ]]; then
   }
 fi
 # end OSX customizations
+
+
+# Git: Make rsa key & copy to clipboard
+make_rsa(){
+  GIT_EMAIL=$(git config --global user.email)
+  echo "\nJust push enter on these\n"
+  # todo: Config email if doesn't exist
+  # todo: return to original directory when done
+  # cd ~/.ssh/
+  ssh-keygen -t rsa -b 4096 -C "$GIT_EMAIL"
+
+  # copy clipboard
+  pbcopy < ~/.ssh/id_rsa.pub  # only works if you use default rsa
+  echo "Copied key to clipboard."
+  echo "Opening browser"
+  open https://github.com/settings/ssh
+
+}
