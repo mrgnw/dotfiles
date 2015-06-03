6891f830c22da627abda9221a3dd1b9fd6d82ac9
Morgan Williams <wowzapollo@gmail.com>
2015-06-03 16:28:02 -0600

    gcnp=git commit new branch. Autocorrect for Meteor

--- meteor.zsh
+++ meteor.zsh
@@ -2,3 +2,12 @@ alias m='meteor'
 
 # for e2e testing
 alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox-bin -p'
+
+
+# autocorrect
+alias metor='meteor'
+alias mteor='meteor'
+alias metor='meteor'
+alias mtor='meteor'
+alias mtr='meteor'
+alias mtor='meteor'
