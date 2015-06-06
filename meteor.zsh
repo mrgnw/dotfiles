3b50d276d0bae281ab92abced358c8ee3862e873
Morgan <code@mrgnw.dev>
2015-06-05 23:55:56 -0600

    mcreate automates creating a project

--- meteor.zsh
+++ meteor.zsh
@@ -1,13 +1,24 @@
 alias m='meteor'
 
-# for e2e testing
-alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox-bin -p'
-
+# Meteor request:
+# Have these as meteor default `meteor create` settings
+# 1. Don't create [project].css/html/js
+# 2. Create client server public private
+mcreate() {
+  meteor create $@;
+  # Q: can I hide the output from this?
+  cd $1
+  echo mkdir client server public private;
+  echo rm $1.*; # removes the default project.css, html, and js
+}
+mcreaterun() { mcreate $@; meteor }
 
 # autocorrect
+alias mtr='meteor'
+alias mtor='meteor'
 alias metor='meteor'
 alias mteor='meteor'
 alias metor='meteor'
-alias mtor='meteor'
-alias mtr='meteor'
-alias mtor='meteor'
+
+# for e2e testing
+alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox-bin -p'
