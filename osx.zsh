171863d8fefe6caf2d011af036718210638c6ea0
Morgan W <morganfwilliams@gmail.com>
2016-12-03 00:05:11 -0700

    Major update to mac init script

--- osx.zsh
+++ osx.zsh
@@ -5,9 +5,9 @@ if [[ $(uname) == "Darwin" ]]; then
 
   alias f='open .'
   alias editHosts='sudo $EDITOR /etc/hosts'
-  alias cdicloud="cd /Users/mw/Library/Mobile\ Documents/com\~apple\~CloudDocs"
-  alias finderShowHidden='defaults write com.apple.finder ShowAllFiles TRUE; killall Finder'
-  alias finderHideHidden='defaults write com.apple.finder ShowAllFiles FALSE; killall Finder'
+  alias icloud="cd /Users/mw/Library/Mobile\ Documents/com\~apple\~CloudDocs"
+  alias showHiddenFiles='defaults write com.apple.finder ShowAllFiles TRUE; killall Finder'
+  alias hideHiddenFiles='defaults write com.apple.finder ShowAllFiles FALSE; killall Finder'
 
   # Merge one folder into another.
   # Like clicking & dragging it in, except it merges files
@@ -15,6 +15,18 @@ if [[ $(uname) == "Darwin" ]]; then
     echo "Merging $(basename $1) into $(basename $2)"
     ditto $@
     trash $1
+  }
+
+  openGate() {
+    echo "Opening the gates"
+    sudo spctl --master-disable
+    defaults write com.apple.LaunchServices LSQuarantine -bool NO
+  }
+
+  closeGate() {
+    echo "Gatekeeper re-activated"
+    sudo spctl --master-enable
+    defaults write com.apple.LaunchServices LSQuarantine -bool YES
   }
 
 
