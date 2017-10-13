56d466525d42ae38ce998688b2d40cc289b0179f
Morgan <code@mrgnw.dev>
2017-10-13 17:14:03 -0400

    fk = thefuck, which does a lot of autocorrect stuff on its own

--- autocorrect.zsh
+++ autocorrect.zsh
@@ -1,8 +1,9 @@
 # Common typos that I don't want to deal with
 # github.com/nvbn/thefuck also does a lot of this
-
+alias fk="fuck"
 alias claer="clear"
 alias gti='git'
+
 
 # todo: Read from a dictionary?
 # {
