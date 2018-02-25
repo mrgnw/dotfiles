b6ea719a6dabcaa37871d9d97045714c6f76b565
Morgan <morganfwilliams@gmail.com>
2018-02-25 17:30:47 -0500

    clearing proj; Structuring Svelte

--- svelte.zsh
+++ svelte.zsh
@@ -1,8 +1,16 @@
+# Base
 alias sv='svelte'
 
+
+# Dev
+
 # The --format iife bit means 'generate an immediately-invoked function expression'
-alias iife='svelte compile --format iife'
+iife() {
+	svelte compile --format iife $1.html > $1.js
+}
 # – this allows us to use the component as a simple <script> tag.
 # (By default, Svelte will create a JavaScript module instead, which is 
 # recommended for more serious applications but requires additional steps.)
 
+
+# Production
