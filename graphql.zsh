fbff69eabfee2b04e284d7a25a4096a4a8413bf5
Morgan Williams <morganfwilliams@gmail.com>
2018-08-01 10:39:22 -0400

    starting zsh for apollo-graphql

---
+++ graphql.zsh
@@ -0,0 +1,25 @@
+
+agq-init() {
+	npm init --yes
+	npm install --save apollo-server graphql
+
+	{
+	  echo "const { ApolloServer, gql } = require('apollo-server');"
+	} > index.js
+
+	# Git init with node gitignore
+	curl https://www.gitignore.io/api/node > .gitignore
+	git init
+	git add .
+	git commit -m '🚀 Init Apollo-GraphQL!'
+
+	open index.js # or `subl .` ?
+}
+
++apollo() {
+	mkdir $1
+	cd $1
+	agq-init
+}
+
+
