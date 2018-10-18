35eedadd315fdfb9bff24595d2a5946dab43238c
Morgan <code@mrgnw.dev>
2018-10-17 22:46:24 -0400

    virtualenv organization

--- python.zsh
+++ python.zsh
@@ -2,15 +2,13 @@ alias py=python3
 alias python=python3
 alias pip=pip3
 
-# 2018 virtualenv setup
-export WORKON_HOME=~/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-
 export PATH="$PATH:/Users/morgan.williams/Library/Python/3.7/bin"
 
 alias jupy='jupyter notebook'
 
 # virtualenv / virtualenvwrapper
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
 mkvenv() { mkvirtualenv --python=/usr/local/bin/python3 $* }
 rmvenv() { rmvirtualenv $* }
 cdvenv() { cdvirtualenv $* }
