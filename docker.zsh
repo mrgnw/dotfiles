ffee3ad24c7caa907d5b98d6ade33bf8bb6335e0
Morgan <code@mrgnw.dev>
2018-12-14 06:22:59 -0500

    trying out docker defaults

---
+++ docker.zsh
@@ -0,0 +1,28 @@
+
+
+dockerPulls(){
+	
+	standardDocks=(
+		linuxserver/radarr
+		linuxserver/sonarr
+		linuxserver/docker-transmission
+		jstewig/docker-vpn-unlimited
+		)
+
+	for d in $standardDocks; do
+		echo "🍺 loading the docks 🚢 🐳" 
+		docker pull $d
+	done
+
+}
+
+# Run a docker command on multiple items
+# e.g. `dkr pull a b` = docker pull a; docker pull b;
+dkr(){
+	docker_command=$1
+	shift
+	for i in "$@"
+	do 
+		docker $docker_command "$i"
+	done
+}
