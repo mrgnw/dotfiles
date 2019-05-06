f2df2df8b6c971ae981f5996136e2db4e409785a
Morgan <code@mrgnw.dev>
2019-05-06 17:48:52 -0400

    cleanup svelte

--- svelte.zsh
+++ svelte.zsh
@@ -1,17 +1,9 @@
 #!/bin/zsh
 # Base
-alias sv='svelte'
 
-
-# Dev
-
-# The --format iife bit means 'generate an immediately-invoked function expression'
-iife() {
-	svelte compile --format iife $1.html > $1.js
+sv() {
+	npm run ${1:-dev}
 }
-# – this allows us to use the component as a simple <script> tag.
-# (By default, Svelte will create a JavaScript module instead, which is 
-# recommended for more serious applications but requires additional steps.)
 
 +sv() {
 	degit sveltejs/template $1
