a78e5a9901c6c140679231a6f04d5a0763f51211
Morgan Williams <morgan.williams@sapphire-digital.com>
2019-06-13 14:03:11 -0400

    aliases zd=zdir zed=zedit

--- zsh.zsh
+++ zsh.zsh
@@ -25,10 +25,12 @@ ZSH_THEME="avit-pure"
 # Plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git gibo osx cp colorize vagrant python pip virtualenv
-  jsontools zsh-syntax-highlighting colored-man themes)
+  jsontools zsh-syntax-highlighting zsh-autosuggestions colored-man themes)
 
-alias zd="cd ~/.oh-my-zsh/custom/"
-alias zed="edit ~/.oh-my-zsh/custom/"
+alias zdir="cd ~/.oh-my-zsh/custom/"
+alias zedit="edit ~/.oh-my-zsh/custom/"
+alias zed=zedit
+alias zd=zdir
 alias dev='cd ~/dev'
 alias typora='open -a typora'
 alias tp='typora'
