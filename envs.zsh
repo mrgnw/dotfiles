600e4199f56127054a880562dde6ac5d25afc918
Morgan <code@mrgnw.dev>
2019-07-29 13:56:38 -0400

    update

--- envs.zsh
+++ envs.zsh
@@ -1,4 +1,4 @@
-# # Lazy load pyenv
+# pyenv
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"  # automatically init
 
@@ -14,30 +14,33 @@ if type pyenv > /dev/null; then
     }
 fi
 
-# nvm
-
-# zplugin ice silent wait'2'; zplugin load "lukechilds/zsh-nvm"
+# Only load in ~/dev?
+zplugin ice wait'0' load'[[ $PWD != $HOME/dev/** ]]' unload'![[ $PWD = $HOME/dev/** ]]';
+zplugin load "lukechilds/zsh-nvm"
 # place this after nvm initialization!
 autoload -U add-zsh-hook
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+# load-nvmrc() {
+#   local node_version="$(nvm version)"
+#   local nvmrc_path="$(nvm_find_nvmrc)"
 
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+#   if [ -n "$nvmrc_path" ]; then
+#     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+#     if [ "$nvmrc_node_version" = "N/A" ]; then
+#       nvm install
+#     elif [ "$nvmrc_node_version" != "$node_version" ]; then
+#       nvm use
+#     fi
+#   elif [ "$node_version" != "$(nvm version default)" ]; then
+#     echo "Reverting to nvm default version"
+#     nvm use default
+#   fi
+# }
+# add-zsh-hook chpwd load-nvmrc
+# load-nvmrc
+
+
 
 # rvm
 if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
