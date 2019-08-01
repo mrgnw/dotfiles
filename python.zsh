7d992149d3388ee5835f9a2549b2dc8152975db6
Morgan Williams <morgan.williams@sapphire-digital.com>
2019-07-31 21:54:26 -0400

    cleanup python

--- python.zsh
+++ python.zsh
@@ -3,23 +3,15 @@ zplugin snippet \
   https://raw.githubusercontent.com/erikced/zsh-pyenv-lazy-load/master/zsh-pyenv-lazy-load.zsh
 
 
+
+
 jupy(){'jupyter notebook'}
 pi(){'pip install'}
 pu(){'pip uninstall'}
 pf(){'pip freeze'}
 
-# 279
-# alias activate='v'
-# alias deactivate='pyenv deactivate'
-
+# allows pip install x[y]
 setopt +o nomatch
-
-# lazy load pyenv
-# pyenv() {
-#   eval "$(command pyenv init -)"
-#   pyenv "$@"
-# }
-
 
 py(){
   if [ "$#" -eq  "0" ]
@@ -35,22 +27,6 @@ py(){
   +v & (curl https://www.gitignore.io/api/python > .gitignore)
 }
 
-# allows things like `pip install vibora[fast]`
-
-
-# 304
-# requires virtualenvwrapper
-# v() {
-#   local VENVNAME=${1:-${PWD##*/}}  #$1 or current dir name
-#   if [ ! -d ~/.pyenv/versions/$VENVNAME ]; then
-#     +v
-#   else
-#     pyenv activate $VENVNAME
-#   fi
-# }
-
 -v() {
   pyenv uninstall ${1:-${PWD##*/}}
 }
-
-# alias venvs='cd ~/.virtualenvs'
