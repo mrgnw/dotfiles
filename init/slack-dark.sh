5579997ff50d9b46a03499b8f098574b3bc82757
Morgan <code@mrgnw.dev>
2019-10-16 21:50:05 -0400

    Slack includes dark mode now

--- init/slack-dark.sh
+++
@@ -1,8 +0,0 @@
-# make sure slack is installed
-cask install slack
-
-# Install without cloning the whole repo
-# github.com/LanikSJ/slack-dark-mode
-curl https://raw.githubusercontent.com/LanikSJ/slack-dark-mode/master/dark-theme.css -o dark-theme.css \
-  & curl https://raw.githubusercontent.com/LanikSJ/slack-dark-mode/master/event-listener.js -o event-listener.js
-curl -L https://raw.githubusercontent.com/LanikSJ/slack-dark-mode/master/slack-dark-mode.sh | sh
