2619258670eaf97b1efc16c49a47a73352260b43
Morgan <morganfwilliams@gmail.com>
2019-10-17 01:30:35 -0400

    .3x faster +zsh-autosuggestions, -nvm

--- node.zsh
+++ node.zsh
@@ -1,3 +1,3 @@
 # lazy load zsh-nvm
-export NVM_LAZY_LOAD=true
-zplugin ice lucid wait"0"; zplugin load "lukechilds/zsh-nvm"
+# export NVM_LAZY_LOAD=true
+# zplugin ice lucid wait"0"; zplugin load "lukechilds/zsh-nvm"
