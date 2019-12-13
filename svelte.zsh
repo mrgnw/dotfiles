f7977de038eb8645704c0937c899208ff8b59bdb
Morgan <morganfwilliams@gmail.com>
2019-12-13 01:14:07 -0500

    fix broken line

--- svelte.zsh
+++ svelte.zsh
@@ -6,7 +6,8 @@ sv() {
 +sv() {
 	degit sveltejs/template "$1"
 	cd "$1"
-	pnpm install code . -g src/App.svelte
+	code . -g src/App.svelte
+	pnpm install
 	pnpm run dev
 	open http://localhost:5000
 }
