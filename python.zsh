2c247f9d4e8d70d71c2e9afb7a64d908616279c7
Morgan Williams <morgan.williams@sapphire-digital.com>
2020-01-22 08:58:41 -0500

    +py works with no arguments too

--- python.zsh
+++ python.zsh
@@ -1,50 +1,54 @@
-zplugin ice OMZ::davidparsson/zsh-pyenv-lazy silent wait"0";
+zplugin ice OMZ::davidparsson/zsh-pyenv-lazy silent wait"0"
 zplugin load davidparsson/zsh-pyenv-lazy
 
-jupy(){'jupyter notebook'}
-pi(){'pip install'}
-pu(){'pip uninstall'}
-pf(){'pip freeze'}
+jupy() {'jupyter notebook'}
+pi() {'pip install'}
+pu() {'pip uninstall'}
+pf() {'pip freeze'}
 
 # allows pip install x[y]
 setopt +o nomatch
 
 # find latest python3 version on pyenv
-PYTHON3_VERSION(){
-  pyenv install -l | grep -e '3.[0-9].[0-9]' | grep -v - | tail -1 | awk '{$1=$1};1'
+PYTHON3_VERSION() {
+	pyenv install -l | grep -e '3.[0-9].[0-9]' | grep -v - | tail -1 | awk '{$1=$1};1'
 }
 
-py(){
-  if [ "$#" -eq  "0" ]
-    then python
-  else
-    python -m $@
-  fi
+py() {
+	if [ "$#" -eq "0" ]; then
+		python
+	else
+		python -m $@
+	fi
 }
 
 # create venv, set as directory default
 +v() {
-  pyenv virtualenv $PYTHON3_VERSION ${1:-${PWD##*/}}
-  pyenv local ${1:-${PWD##*/}}
-  # I basically always need cython
-  pip install cython
+	pyenv virtualenv $PYTHON3_VERSION ${1:-${PWD##*/}}
+	pyenv local ${1:-${PWD##*/}}
+	# I basically always need cython
+	pip install cython
 }
 
 v() {
-  local VENVNAME=${1:-${PWD##*/}}  #$1 or current dir name
-  if [ ! -d ~/.pyenv/versions/$VENVNAME ]; then
-    +v
-  else
-    pyenv activate $VENVNAME
-  fi
+	local VENVNAME=${1:-${PWD##*/}} #$1 or current dir name
+	if [ ! -d ~/.pyenv/versions/$VENVNAME ]; then
+		+v
+	else
+		pyenv activate $VENVNAME
+	fi
 }
 
 +py() {
-  mkdir $1;
-  cd $1;
-  +v & (curl https://www.gitignore.io/api/python > .gitignore)
+	# if no args:
+	if [ "$#" -ge 1 ]; then
+		mkdir $1
+		cd $1
+	fi
+	+v &
+	(curl https://www.gitignore.io/api/python >.gitignore)
 }
 
 -v() {
-  pyenv uninstall ${1:-${PWD##*/}}
+	pyenv uninstall ${1:-${PWD##*/}}
 }
