4600234d2f7e1eeb43ee74428d27bcb32192bb65
Morgan <code@mrgnw.dev>
2020-07-24 18:55:38 -0400

    Fix edit

--- custom/z.zsh
+++ custom/z.zsh
@@ -1,12 +1,10 @@
 setopt autocd
-setopt auto_list
-setopt auto_menu
-setopt correct
-setopt auto_param_keys
-setopt hist_ignore_dups
-setopt interactive_comments
 
-export GREP_OPTIONS='--color=always'
+zplugin ice OMZ::zsh-users/zsh-autosuggestions silent wait"0";
+zplugin load zsh-users/zsh-autosuggestions
+
+# zplugin ice OMZ::zsh-users/zsh-history-substring-search silent wait"0";
+# zplugin load zsh-users/zsh-history-substring-search
 
 # cd into the file's directory if it's a file
 function cd() {
@@ -18,7 +16,7 @@ function cd() {
 }
 
 export EDITOR=code
-e(){
+edit(){
   $EDITOR $@
 }
 
@@ -40,11 +38,15 @@ alias lsl='ls -l --block-size=M'
 ❯(){""}
 export ZDIR="$HOME/.zsh"
 alias zd="cd $ZDIR"
-alias zed="e $ZDIR"
+alias zed="edit $ZDIR"
 
 +d() { mkdir -p $1; cd $1 }
 
-zz(){zpl update -q}  # ; source $HOME/.zshrc
+zz(){zplg update}  # ; source $HOME/.zshrc
+
+# highlighters
+zplugin ice lucid wait"0";
+zplugin load zdharma/fast-syntax-highlighting
 
 # simple ping function
 simpleping() {
@@ -60,10 +62,23 @@ sping() { simpleping $@ }
 spg() { sp google.com }
 sp8() { sp 8.8.8.8 }
 
-rand(){
-  python -c "from random import randrange; print(randrange(0, ${1:10}))"
+daily(){
+  RAND_MIN=$(rand 30)
+  echo "10 $RAND_MIN * * 1-5"
+  (crontab -l ; echo "SHELL=/bin/zsh; 10 $RAND_MIN * * 1-5 $@") | crontab
+  crontab -l
 }
 
-ksh(){( klist -s || kinit) && ssh $@}
-kshtail(){ ksh -t $1 "tail -f $2" }
-killport(){ lsof -ti:$1 | xargs kill }
+rand(){
+  python -c "from random import randrange; print(randrange(0, $1))"
+}
+
+ksh() {(klist -s || kinit) && ssh $@}
+kshtail() {
+  ksh -t $1 "tail -f $2"
+}
+
+
+killport(){
+  lsof -ti:$1 | xargs kill
+}
