836fdca249b80d527859f19de192a5b24e7eb8fd
Morgan <code@mrgnw.dev>
2020-07-24 18:55:51 -0400

    espanso install

---
+++ init/espanso.sh
@@ -0,0 +1,10 @@
+# my package
+espanso install espansos https://github.com/mrgnw/espansos --external
+
+# community packages
+espanso install basic-emojis
+espanso install mac-symbols
+espanso install misspell-en_US --external
+
+espanso register
+# espanso restart
