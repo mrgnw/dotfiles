4eeef1cf7b9a97401684420b525fd507bc9ff214
Morgan <code@mrgnw.dev>
2020-07-27 01:16:11 -0400

    cleanup

--- z.zsh
+++ z.zsh
@@ -1,11 +1,6 @@
 setopt autocd
 setopt interactivecomments
 
-zplugin ice OMZ::zsh-users/zsh-autosuggestions silent wait"0";
-zplugin load zsh-users/zsh-autosuggestions
-
-# zplugin ice OMZ::zsh-users/zsh-history-substring-search silent wait"0";
-# zplugin load zsh-users/zsh-history-substring-search
 
 # cd into the file's directory if it's a file
 function cd() {
@@ -45,10 +40,6 @@ alias zed="edit $ZDIR"
 
 zz(){zpl update -q}  # ; source $HOME/.zshrc
 
-# highlighters
-zplugin ice lucid wait"0";
-zplugin load zdharma/fast-syntax-highlighting
-
 # simple ping function
 simpleping() {
   ping $1 | awk \
@@ -63,23 +54,10 @@ sping() { simpleping $@ }
 spg() { sp google.com }
 sp8() { sp 8.8.8.8 }
 
-daily(){
-  RAND_MIN=$(rand 30)
-  echo "10 $RAND_MIN * * 1-5"
-  (crontab -l ; echo "SHELL=/bin/zsh; 10 $RAND_MIN * * 1-5 $@") | crontab
-  crontab -l
-}
-
 rand(){
-  python -c "from random import randrange; print(randrange(0, $1))"
+  python -c "from random import randrange; print(randrange(0, ${1:10}))"
 }
 
-ksh() {(klist -s || kinit) && ssh $@}
-kshtail() {
-  ksh -t $1 "tail -f $2"
-}
-
-
-killport(){
-  lsof -ti:$1 | xargs kill
-}
+ksh(){( klist -s || kinit) && ssh $@}
+kshtail(){ ksh -t $1 "tail -f $2" }
+killport(){ lsof -ti:$1 | xargs kill }
