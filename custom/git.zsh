e804236a733f749473c2b8a8cd8689791339ada4
Morgan <code@mrgnw.dev>
2020-08-01 04:17:17 -0400

    mrgnw/git

--- custom/git.zsh
+++ custom/git.zsh
@@ -1,20 +1,5 @@
 #!/bin/zsh
-# git: set global gitignore to the one I have in my zsh custom folder
-alias setglobalgitignore='git config --global core.excludesfile ~/.oh-my-zsh/custom/.gitignore_global'
-
-alias gp='git pull'
-alias gs='git status'
-alias gc='git commit -m'
-alias gcnb='git checkout -b'
 alias gits='git status'
-alias gita='git add --all'
-alias gitc='git commit -m'
-alias gitglobal='git config --global --edit'
-alias giglobal='code ~/.zsh/.gitignore_global'
-
-# gitp ush
-alias gitp='git push;'
-alias ush=''
 
 rsa() {
   if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
@@ -23,30 +8,4 @@ rsa() {
     pbcopy <~/.ssh/id_rsa.pub
   fi
   echo "rsa copied"
-}
-
-# custom git command prototypes
-
-yolo() {
-  # Only run on main
-  git add --all
-  git commit -m 'quick update'
-  git push
-}
-
-clear_merged_branches(){
-  git branch -d $(git branch --merged=master | grep -v master)
-}
-
-clear_old_branches() {
-  git branch -D $(git branch --sort=-committerdate |
-                  grep -v 'master\|main' |
-                  tail +16 |
-                  tr '\n' ' '
-                )
-}
-
-extract_untracked() {
-  rsync -R $(git ls-files --others) "$1"
-  git clean -f
 }
---
+++ init/git.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/mrgnw/git ~/.config/git
