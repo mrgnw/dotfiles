d9b74c5a6e659c988e71548c0ad7bea88460a532
Morgan Williams <code@mrgnw.dev>
2020-08-01 16:09:41 -0400

    pyenv-latest plugin

--- init/py.sh
+++ init/py.sh
@@ -4,3 +4,5 @@
 b+ pyenv pyenv-virtualenv
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
+# xxenv-latest
+git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
