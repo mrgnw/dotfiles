6450d1bad9e13e338f40d11fdcd44e9077252030
Morgan Williams <m1@m1-4.local>
2021-03-22 23:57:02 -0400

    Delete py.sh

--- init/py.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# pipx
-b+ pyenv pyenv-virtualenv
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-# xxenv-latest
-git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
-pip install --upgrade pip
