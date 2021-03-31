c3e01220ffec45d3c6a7f6492c21a47379c4ed80
Morgan Williams <m1@m1-4.local>
2021-03-31 03:44:50 -0400

    Update linux.sh

--- init/linux.sh
+++ init/linux.sh
@@ -2,28 +2,26 @@ is_linux || return 1
 
 apps=(
     curl
+    openssh-server
     flatpak
-    gnome-tweaks
-    gnome-software-plugin-flatpak
     snapd
-    gnome-software-plugin-snap
+    docker-compose
     kde-plasma-desktop
-
 )
+
 sudo apt-get update
 sudo apt-get install -y $apps
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 
  apps2=(
- 	obs-studio  
+ 	obs-studio
 	lutris
  )
  sudo apt-get install -y $apps2
 
 # flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpaks=(
     Discord
     spotify
@@ -39,3 +37,8 @@ flatpak install $flatpaks
 # snap
 snap install --edge 1password
 snap install whitesur-gtk-theme
+
+# ssh
+sudo ufw allow ssh
+# don't show login message to ssh users
+touch ~/.hushlogin
