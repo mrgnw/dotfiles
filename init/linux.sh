56ff24aae4e3fd750ecc1b7e816f5c56794b2715
Morgan <code@mrgnw.dev>
2021-07-23 02:43:49 -0400

    tabs

--- init/linux.sh
+++ init/linux.sh
@@ -1,14 +1,15 @@
 is_linux || return 1
 
 apps=(
-    bat   # pretty cat
-    curl
-    delta
-    dust
-    openssh-server
-    flatpak
-    docker-compose
-    ncdu  # disk space analyzer
+	bat   # pretty cat
+	curl
+	delta
+	dust
+	gping
+	openssh-server
+	flatpak
+	docker-compose
+	ncdu  # disk space analyzer
 )
 
 sudo apt update
@@ -18,24 +19,24 @@ sudo apt install -y $apps
 
 brew install jq tgotwig/linux-dust
 
- apps2=(
- 	obs-studio
+apps2=(
+	obs-studio
 	lutris
-    latte-dock
- )
- sudo apt install -y $apps2
+	latte-dock
+)
+sudo apt install -y $apps2
 
 # flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpaks=(
-    Discord
-    spotify
-    com.visualstudio.code
-    typora
-    tangram
-    GreenWithEnvy
-    Spotify
-    sublimemerge
+	Discord
+	spotify
+	com.visualstudio.code
+	typora
+	tangram
+	GreenWithEnvy
+	Spotify
+	sublimemerge
 )
 flatpak install $flatpaks
 
