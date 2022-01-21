d390f478cc14aa9a81fa499be40d29776622a49b
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-01-20 23:02:56 -0800

    install dev basics with asdf

---
+++ init/dev.sh
@@ -0,0 +1,43 @@
+brew install asdf
+
++asdf() {
+  asdf plugin-add $1
+  asdf install $1 ${2:-latest}
+  asdf global $1 ${2:-latest}
+}
++asdfs(){ for i in $@; do +asdf $i done
+}
+
+asdf plugin list all
+
+runtimes=(
+	python
+	nodejs
+	pnpm
+	golang
+	redis
+)
++asdfs $runtimes
+
+clis=(
+	dust
+	duf
+	gojq
+	httpie-go
+	ripgrep
+	trdsql  # query csv, json, etc — w/ PostgreSQL syntax
+)
++asdfs $clis
+
+# swift
+# v
+# rust
+# ruby
+
+# 1password-cli
+# exa
+# redis-cli
+# usql
+# zoxide
+# postgres
+
