5f6d4b45c04fa737a8ef6241a9988c0964a22c73
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-02-23 01:48:30 -0800

    trying asdf for go version management

--- custom/go.zsh
+++ custom/go.zsh
@@ -1,18 +1,2 @@
-
 export GOPATH="$HOME/go"
 PATH="$GOPATH/bin:$PATH"
-
-upgrade_go(){
-	local version="${1:-latest}"
-	(g install $version && g set $version && g list)
-}
-+golang(){
-	upgrade_go \
-	|| (curl -sSL https://git.io/g-install | sh -s) \
-	   && upgrade_go
-}
-
-"»g"(){
-	g self-upgrade
-}
-
