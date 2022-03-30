2430b02553fc559a02e238cbd239be02bb4e3d54
Morgan Williams <2504532+mrgnw@users.noreply.github.com>
2022-03-30 14:25:46 -0700

    custom settings for ssh sessions: EDITOR=micor

---
+++ custom/ssh.zsh
@@ -0,0 +1,13 @@
+# user micro editor when session is ssh
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  # add these customizations
+  SESSION_TYPE=remote/ssh
+  # echo "ahoy! $SESSION_TYPE"
+  EDITOR=micro
+else
+  case $(ps -o comm= -p "$PPID") in
+    sshd|*/sshd) SESSION_TYPE=remote/ssh;;
+  esac
+fi
+
+# https://unix.stackexchange.com/a/9607/77873
