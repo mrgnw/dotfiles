22c326ac58dc8a9aed8373bbd933264116fd38ae
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-04-08 17:23:10 -0700

    Update git.zsh

--- custom/git.zsh
+++ custom/git.zsh
@@ -4,15 +4,6 @@ alias gd='cd ~/.config/git'
 alias ged='code ~/.config/git'
 alias gedl='git config --edit'
 
-rsa() {
-  if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
-    ssh-keygen -t rsa -b 4096 -C morganfwilliams@gmail.com
-  else
-    pbcopy <~/.ssh/id_rsa.pub
-  fi
-  echo "rsa copied"
-}
-
 deg() {
     degit $@
     cd $1
