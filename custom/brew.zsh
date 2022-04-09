56a803d7ffd6b25400b9d2c54af55529376fbab0
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-04-08 17:13:36 -0700

    simplify brews

--- custom/brew.zsh
+++ custom/brew.zsh
@@ -3,7 +3,7 @@ alias cask='brew cask'
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_AUTO_UPDATE_SECS=300
-export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
+export HOMEBREW_CLEANUP_MAX_AGE_DAYS=15
 export HOMEBREW_DISPLAY_INSTALL_TIMES=1
 export HOMEBREW_NO_GITHUB_API=1  # exclude issues in brew search
 
@@ -13,19 +13,6 @@ brews() {
 }
 bs(){ brew search $@ }
 # parallel brew/cask
+b+(){ brew install "$@" }
 b-(){ echo "${@}" | xargs -n 1 -P 9 -I '{}' zsh -c 'brew uninstall {}' }
-b»(){ echo "${@}" | xargs -n 1 -P 9 -I '{}' zsh -c 'brew reinstall {}' }
-c+(){ echo "${@}" | xargs -n 1 -P 9 -I '{}' zsh -c 'brew install --cask {}'}
-c-(){ echo "${@}" | xargs -n 1 -P 9 -I '{}' zsh -c 'brew uninstall --cask {}' }
-
-# install @ 10x parallel download
-b+() {
-	for INSTALL in "$@"; do
-		brew deps $INSTALL |
-			xargs -n 1 -P 10 -I {} -r zsh -c 'brew fetch {}'
-	done \
-	&
-	for INSTALL in "$@"; do
-		brew install $INSTALL
-	done
-}
+b»(){ brew update; brew upgrade; brew doctor; brew cask upgrade; brew cleanup; }
