9e635fcf3374f30d9a30bdb3dd250831a877417b
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-04-12 12:16:13 -0700

    Update brew.zsh

--- custom/brew.zsh
+++ custom/brew.zsh
@@ -19,13 +19,13 @@ b+(){
 		echo "brew update; brew upgrade"
 		brew update; brew upgrade; brew cleanup;
 	fi
-	}
+}
 b-(){
 	if [[ $# -gt 0 ]]; then
 		brew uninstall "$@"
 	else
 		brew cleanup -s
 	fi
-	}
+}
 bs(){ brew search $@ }
 b^(){ brew outdated $@ | column -t -s ' '; }
