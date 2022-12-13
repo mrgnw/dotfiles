c1e754fcd3ba922e546a6a3f7ad0e26d20ece105
Morgan <2504532+mrgnw@users.noreply.github.com>
2022-12-13 00:51:14 -0700

    Update video.zsh

--- video.zsh
+++ video.zsh
@@ -1,4 +1,3 @@
-
 hevc(){
     local filename=$1:t:r
     ffmpeg -i $1 -c:v hevc_videotoolbox -b:v 2M $filename.mp4
@@ -11,7 +10,18 @@ atv(){
     # $2 accepts quality - 1080p60, 1080p30, 720p60, 720p30, 480p30, 360p30, 240p30, 144p30
     local quality=${2:-"1080p30"}
 
-    handbrake -i "$1" -o "${1%.*}_$quality.mp4" --preset "Apple $quality Surround" --all-subtitles --all-audio
+    fname=$1:t:r
+    input=$1
+    local output_dir=$HANDBRAKE_OUTPUT_DIR
+    # if HANDBRAKE_OUTPUT_DIR is not set, use current directory
+    if [[ -z $HANDBRAKE_OUTPUT_DIR ]]; then
+        output_dir=$(pwd)
+    fi
+    echo output_dir "$output_dir"
+
+    handbrake --input "$input" -o "$output_dir/$fname-$quality.mp4" \
+        --preset "Apple $quality Surround" \
+        --all-subtitles --all-audio --optimize
     # ensure format will play in macos quicklook using handbrake
     # ffmpeg -i input.mp4 -c:v h264_videotoolbox -b:v 2M -hwaccel videotoolbox output.mp4
     # ffmpeg -i "${1%.*}.mp4" -c copy -movflags faststart "${1%.*}.mp4"
@@ -19,38 +29,14 @@ atv(){
  
  atvs(){
     # apply profile 'Apple 1080p60 HEVC Surround' to all files in current directory
-    # local quality=${2:-"1080p30"}
+    local quality=${2:-"1080p30"}
     for f in *; 
+        # get file extension
+        # local ext=${f##*.}
         # before applying profile, check if file is already Apple-optimized
         if [[ $(mediainfo --Inform="Video;%Format%" "$f") != "HEVC" ]]; then
-            atv "$f"
+            atv "$f" "$quality"
         else
             echo " - $f is already optimized"
         fi
- }
-
-
- ff(){
-    # encode with hevc and move english audio track to first
-    ffmpeg -i $1 -c:v hevc_videotoolbox -b:v 1M -c:a aac -b:a 128k -ac 2 -ar 44100 -movflags +faststart "${1%.*}.mp4"
-    # swap audio tracks
-    # ffmpeg -i $1.mp4 -map 0:0 -map 0:2 -map 0:1 -c copy $1.mp4
- }
-
- ffs(){
-    # run ffmpeg on all files in current directory if they're not already hevc
-    for f in *; do
-        # before applying profile, check if file is already Apple-optimized
-        local codec=$(mediainfo --Inform="Video;%Format%" "$f")
-        if [[ $codec != "HEVC" ]]; then
-            ff "$f"
-        else
-            # if extension is not mp4, convert to mp4
-            echo " - $f is already optimized to $codec."
-            if [[ $f != *.mp4 ]]; then
-                echo "Moving to mp4" &&
-                mv "$f" "${f%.*}.mp4"
-            fi
-        fi
-        done
  }
