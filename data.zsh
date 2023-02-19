e837682ec09a4221d851895408286cc38c4a2bee
Morgan <2504532+mrgnw@users.noreply.github.com>
2023-02-19 01:47:00 -0300

    use dasel output in another format (xml to json)

--- data.zsh
+++ data.zsh
@@ -1,5 +1,11 @@
 alias duck='duckdb'
 
+# dasf file [new_ext]
+dasf(){
+  new_ext="${2:-json}"
+  dasel -f "$1" -r "$1:e" -w "$new_ext" > "$1:r"."$new_ext" 
+}
+
 xls(){
 	# if there's one argument, convert it to csv
 	if [[ $# -gt 0 ]]; then
