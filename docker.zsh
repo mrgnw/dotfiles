eba090f336ad2e811f27bf0e2b16d224646ef852
Morgan <2504532+mrgnw@users.noreply.github.com>
2023-06-05 14:16:57 -0300

    Update docker.zsh

--- docker.zsh
+++ docker.zsh
@@ -19,18 +19,6 @@ dc(){
     fi
 }
 
-
-# Run a docker command on multiple items
-# e.g. `dkr pull a b` = docker pull a; docker pull b;
-dkdk(){
-	docker_command=$1
-	shift
-	for i in "$@"
-	do
-		docker $docker_command "$i"
-	done
-}
-
 comp(){ composerize "$@" | pbcopy }
 
 # docker-compose auto-completion
