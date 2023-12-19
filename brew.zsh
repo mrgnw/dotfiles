a8ff06d678d84c8a629645df5b89a7db36523396
Morgan <2504532+mrgnw@users.noreply.github.com>
2023-12-18 19:25:02 -0700

    use brew.env
    $HOMEBREW_PREFIX/etc/homebrew/brew.env

--- brew.zsh
+++ brew.zsh
@@ -1,18 +1,12 @@
 #!/bin/zsh
 alias cask='brew cask'
-
-export HOMEBREW_AUTO_UPDATE_SECS=300
-export HOMEBREW_CLEANUP_MAX_AGE_DAYS=15
-export HOMEBREW_DISPLAY_INSTALL_TIMES=1
-export HOMEBREW_INSTALL_FROM_API=1
-export HOMEBREW_NO_GITHUB_API=1  # exclude issues in brew search
-export HOMEBREW_INSTALL_BADGE="🍦"
+alias b='brew'
 
 # list installed homebrews name & desc
 brews() {
 	brew info --json=v1 --installed | jq -r '. [] | "\(.name)||\(.desc)"' | column -t -s '||';
 }
-alias b='brew'
+
 b+(){
 	if [[ $# -gt 0 ]]; then
 		brew install "$@"
@@ -21,6 +15,7 @@ b+(){
 		brew update; brew upgrade; brew cleanup;
 	fi
 }
+
 b-(){
 	if [[ $# -gt 0 ]]; then
 		brew uninstall "$@"
@@ -29,5 +24,6 @@ b-(){
 		brew cleanup -s
 	fi
 }
+
 bs(){ brew search $@ }
 b^(){ brew outdated $@ | column -t -s ' '; }
