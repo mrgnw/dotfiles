46c5d62f576109d4956ed4f30d20c96f64f94bb1
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-05-19 21:13:52 +0200

    use uv for pip / env

--- python.zsh
+++ python.zsh
@@ -1,23 +1,15 @@
-# zinit ice OMZ::davidparsson/zsh-pyenv-lazy silent wait"0b"
-# zinit load davidparsson/zsh-pyenv-lazy
+p+() { uv pip install "$@"}
+p-() { uv pip uninstall "$@"}
 
-p+() {pip install "$@"}
-p-() {pip uninstall "$@"}
-
-alias jupy='jupyter notebook'
-
-# activate env
-»py() {
-    # activate .venv or create venv
-    if [[ -d .venv ]];
-    then source .venv/bin/activate
-    else
-        python -m venv .venv
-        source .venv/bin/activate
-    fi
+»(){ 
+	if [ -d .venv ]; then
+		source .venv/bin/activate
+	else
+		echo "\t» ~/.venv"
+		source ~/.venv/bin/activate
+	fi
 }
 «(){ deactivate }
--v() { trash .venv}
 
 # allows pip install x[y]
 set +o nomatch
