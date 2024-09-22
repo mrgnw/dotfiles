a96a4b23fe5cfac9bfa8706c2441b8937de9dab2
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-09-22 18:35:27 +0200

    Update uv.zsh

--- uv.zsh
+++ uv.zsh
@@ -2,6 +2,9 @@ t+(){ uv tool install "$@" }
 t»(){ uv tool upgrade "$@" }
 t-(){ uv tool remove "$@" }
 
+
+alias py="uv run python"
+alias uvp="uv run pip"
 # uv autocomplete
 eval "$(uv generate-shell-completion zsh)"
 
