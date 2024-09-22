5d76768a4bfec20f53bfff191003ca29ff82cd4b
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-09-22 20:42:37 +0200

    save template locally to .repos directory

--- svelte.zsh
+++ svelte.zsh
@@ -1,15 +1,34 @@
-»s() { pnpm run ${1:-dev} }
+»s() { bun run ${1:-dev} }
+s+(){ svelte-add $@ }
 
-# create project
 +s() {
+	local appname="${1:-sv$(word)}"
+	local local_template="$HOME/.repos/shadcn-svelte-template"
+	local repo_url="https://github.com/mrgnw/shadcn-svelte-template"
+
+	if [ -d "${local_template}/.git" ]; then
+		git -C "$local_template" pull
+	else
+		mkdir -p "$HOME/.repos"
+		git clone --depth=1 "$repo_url" "$local_template"
+	fi
+
+	cp -R "${local_template}/." "$appname"
+	chmod -R u+rw "$appname"
+	
+	cd "$appname" &&
+		rm -rf .git &&
+		bun install &&
+		bun run dev	
+}
+
+# create a remote repo directly from the template
+s_repo() {
 	appname="${1:-sv$(word)}"
 	gh repo create $appname \
-	  --template https://github.com/mrgnw/shadcn-svelte-template \
-	  --private --clone
+		--template https://github.com/mrgnw/shadcn-svelte-template \
+		--private --clone
 	cd $appname
 	bun install
 	bun run dev
 }
-
-# svelte-add to project
-s+(){ svelte-add $@ }
