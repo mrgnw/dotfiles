31faab81fdae0cce28b4919a5eb868ce93999005
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-05 01:43:40 +0100

    uv autocomplete

--- uv.zsh
+++ uv.zsh
@@ -8,3 +8,18 @@ alias uvp="uv run pip"
 # uv autocomplete
 eval "$(uv generate-shell-completion zsh)"
 
+_uv_run_mod() {
+    if [[ "$words[2]" == "run" && "$words[CURRENT]" != -* ]]; then
+        local venv_binaries
+        if [[ -d .venv/bin ]]; then
+            venv_binaries=( ${(@f)"$(_call_program files ls -1 .venv/bin 2>/dev/null)"} )
+        fi
+        
+        _alternative \
+            'files:filename:_files' \
+            "binaries:venv binary:(($venv_binaries))"
+    else
+        _uv "$@"
+    fi
+}
+compdef _uv_run_mod uv
