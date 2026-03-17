252277d025c80e4f6521fe80327cf073732a4dbd
Morgan Williams <2504532+mrgnw@users.noreply.github.com>
2026-03-17 13:05:26 +0100

    ssh: show spring green ✦ prompt for remote sessions

--- ssh.zsh
+++ ssh.zsh
@@ -5,13 +5,12 @@ sshake(){
 
 # user micro editor when session is ssh
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  # add these customizations
   SESSION_TYPE=remote/ssh
-  # echo "ahoy! $SESSION_TYPE"
   EDITOR=micro
+  SHELL_SYMBOL="%F{#05ffa1}✦%f"
 else
   case $(ps -o comm= -p "$PPID") in
-    sshd|*/sshd) SESSION_TYPE=remote/ssh;;
+    sshd|*/sshd) SESSION_TYPE=remote/ssh; SHELL_SYMBOL="%F{#05ffa1}✦%f";;
   esac
 fi
 
